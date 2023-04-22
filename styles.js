32d67a9dd3a22453803edf4f244dc84ab97131b8
Andrés Pujol <andrespujol@gmail.com>
2023-04-22 18:37:49 -0300

    Incorporé eventos para agregar y eliminar tareas

--- styles.js
+++ styles.js
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 
-
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -24,5 +23,60 @@ export const styles = StyleSheet.create({
       width: '70%',
       paddingHorizontal: 5,
       marginRight: 5
+    },
+    listContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      marginVertical: 20,
+  },
+    itemContainer:{
+      height: 80,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: '#3481AE',
+      borderRadius: 5,
+      margin: 20,
+      padding:10
+    },
+    item:{
+      marginHorizontal: 10,
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    modalContainer:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 60,
+      paddingVertical: 20,
+    },
+    modalTitle:{
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    modalbody:{
+        paddingVertical: 20,
+    },
+      modalMessage:{
+      fontSize: 14,
+      color: '#212121',
+    },
+    modalSelectedEvent:{
+      fontSize: 14,
+      color: '#212121',
+      fontWeight: 'bold',
+      paddingVertical: 10,
+      textAlign: 'center',
+      marginBottom: 20,
+    },
+    modalButtonsContainer:{
+      width: "80%",
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    modalButton: {
+
     },
   });
